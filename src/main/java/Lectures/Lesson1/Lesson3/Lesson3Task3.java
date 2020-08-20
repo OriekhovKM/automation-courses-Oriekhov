dcaeaf5547304efeb59711058c70dd2aabc1c650
OriekhovKM <kiradnepr@gmail.com>
2020-08-20 18:48:49 +0300

    add LT3

---
+++ src/main/java/Lectures/Lesson1/Lesson3/Lesson3Task3.java
@@ -0,0 +1,19 @@
+package Lectures.Lesson1.Lesson3;
+
+public class Lesson3Task3 {
+    public static void main(String[] args) {
+        //Random new = new random 10;
+        //int a = random.nextInt(bound:10);
+        int a=7;
+        if (a > 5){
+            System.out.println("var a > 5");
+            System.out.println("test msg");
+        } else if (a < 2 && a < 5) {
+            System.out.println("var a < 2 && a < 5");
+        } else {
+            System.out.println("var a =" +a);
+        }
+
+
+    }
+}
