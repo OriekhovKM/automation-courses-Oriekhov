a4f7c88ba2731430d64074f55479550778f5019c
OriekhovKM <kiradnepr@gmail.com>
2020-08-20 17:39:57 +0300

    add LT2

---
+++ src/main/java/Lectures/Lesson1/Lesson3/Lesson3Task2.java
@@ -0,0 +1,12 @@
+package Lectures.Lesson1.Lesson3;
+
+public class Lesson3Task2 {
+    public static void main(String[] args) {
+        double Grn = 113;
+        Double Eur = Grn / 32.5;
+        Double Dol = Grn / 27;
+
+        System.out.println(Eur);
+        System.out.print(Dol);
+    }
+}
