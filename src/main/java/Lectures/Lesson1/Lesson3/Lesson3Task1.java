1443e55eea2a676a5368e011df8539aba54202ab
OriekhovKM <kiradnepr@gmail.com>
2020-08-20 17:36:32 +0300

    add LT1

---
+++ src/main/java/Lectures/Lesson1/Lesson3/Lesson3Task1.java
@@ -0,0 +1,13 @@
+package Lectures.Lesson1.Lesson3;
+
+public class Lesson3Task1 {
+    public static void main(String[] args) {
+        double Price1 = 113;
+        double Price2 = 130;
+
+        double CountTotalPrice = (Price1 + Price2) - (Price1 + Price2)*0.07;
+        double TotalPrice = CountTotalPrice+Price2*2;
+
+        System.out.print(TotalPrice);
+    }
+}
