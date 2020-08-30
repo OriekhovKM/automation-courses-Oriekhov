d3008c1720c08727592ba4210dcacd77f913b808
OriekhovKM <kiradnepr@gmail.com>
2020-08-30 08:59:17 +0300

    add lesson 5

---
+++ src/main/java/Lectures/Lesson_5/Task1.java
@@ -0,0 +1,17 @@
+package Lectures.Lesson_5;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String p = "#";
+        for (int i = 0; i < 9; i++) {
+
+            System.out.print(p +"\n");
+            p = p+"#";
+           // for (int j = 0; j < 9; j++) {
+          //      p = p+"#";
+          //      System.out.print(p +"\n");
+          //  }
+        }
+
+    }
+}
