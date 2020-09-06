3d424f75fa8c3dee186c4a95c0f7e86ed925e19f
OriekhovKM <kiradnepr@gmail.com>
2020-09-06 17:32:36 +0300

    HomeWork_5, Task2 was corrected. Please check and merge.

--- src/main/java/HomeWorks/HomeWork_5/Task2.java
+++ src/main/java/HomeWorks/HomeWork_5/Task2.java
@@ -1,5 +1,6 @@
 package HomeWorks.HomeWork_5;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,31 +16,37 @@ public class Task2 {
     System.out.println("Enter c:");
     double c = scanner.nextDouble();
 
-    //quadratic(a, b , c);
-
-        String results = Arrays.toString(quadratic(a, b , c));
-        System.out.println("The equation has following solution " +results);
+    String result = quadratic(a, b , c);
+    System.out.println("The equation has following solution: " +result);
 }
-    private static double [] quadratic (double a, double b, double c) {
+    private static String quadratic (double a, double b, double c) {
         double x1;
         double x2;
         double x;
         double D;
         double results [] = new double [2];
+        String strResults;
 
-        D = Math.pow(b, 2) - 4 * a * c;
+        if (a == 0) {
+            return strResults = "Division by zero. Check parameter a";
+        } else D = Math.pow(b, 2) - 4 * a * c;
+
+
         if (D>0) {
             x1 = (-b+Math.sqrt(D))/(2*a);
             x2 = (-b-Math.sqrt(D))/(2*a);
             results [0] = x1;
             results [1] = x2;
-            return  results;
+            strResults = Arrays.toString(results);
+            return  strResults;
 
         } else if (D == 0) {
             x = -b/2*a;
             results[0] = x;
-            return results;
-        }else return results;
+            strResults = String.valueOf(results[0]);
+            return strResults;
+        } else     return strResults = "The equation has no solutions"; //if D<0
+
     }
 
 }
