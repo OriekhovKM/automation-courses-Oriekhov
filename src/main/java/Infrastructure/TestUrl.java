1b2b811efd2826b19d3485afe9658566bb52ec9b
OriekhovKM <kiradnepr@gmail.com>
2020-09-28 21:59:31 +0300

    Remarks for TestUrl class were fixed. Also included input data verification

--- src/main/java/Infrastructure/TestUrl.java
+++ src/main/java/Infrastructure/TestUrl.java
@@ -7,110 +7,110 @@ public class TestUrl {
     private String path;
     private String param;
 
-    public static class Builder {
-        private String protocol;
-        private String domain;
-        private String port;
-        private String path;
-        private String param;
 
-        public Builder withProtocol (String protocol) {
-            this.protocol = protocol;
+    private TestUrl (Builder builder) {
+        this.protocol = builder.protocol;
+        this.domain = builder.domain;
+        this.port = builder.port;
+        this.path = builder.path;
+        this.param = builder.param;
+
+    }
+
+    @Override
+    public String toString() {
+        return "TestUrl: " +this.protocol +this.domain +this.port +this.path + this.param;
+    }
+
+    public static class Builder {
+        private String protocol = "";
+        private String domain = "";
+        private String port = "";
+        private String path = "";
+        private String param = "";
+
+
+        public Builder withProtocol(String protocol) {
+            if (protocol.contains("://")) {
+                this.protocol = protocol;
+            } else this.protocol = protocol + "://";
             return this;
         }
 
-        public Builder withDomain (String domain) {
-            this.domain = domain;
+        public Builder withDomain(String domain) {
+            if (domain.contains(":")) {
+                this.domain = domain;
+            } else this.domain = domain + ":";
             return this;
         }
 
         public Builder withPort(String port) {
-            this.port = port;
+            if (port.contains("/")) {
+                this.port = port;
+            } else this.port = port + "/";
             return this;
         }
 
         public Builder withPath(String path) {
-            this.path = path;
+            if (path.contains("/")) {
+                this.path = path;
+            } else this.path = path + "/";
             return this;
         }
 
         public Builder withParam(String param) {
-            this.param = param;
+            if (param.contains("?")) {
+                this.param = param;
+            } else this.param = param + "?";
             return this;
         }
 
-        public Builder withParam(String param, String param1) {
-            this.param = param;
-            this.param = param1;
+        public Builder withParam(String param1, String param2) {
+            if (param1.contains("=")) {
+                this.param = param + param1 + param2;
+            } else this.param = param + param1 + "=" + param2;
             return this;
         }
 
-        public TestUrl build(){
-            TestUrl url = new TestUrl();
-            url.protocol = this.protocol;
-            url.domain = this.domain;
-            url.port = this.port;
-            url.path = this.path;
-            url.param = this.param;
-
+        public String build() {
+            String url = String.valueOf(new TestUrl(this));
             return url;
         }
 
 
-    }
-    private TestUrl() {
+        public static void main(String[] args) {
+            String url = new TestUrl.Builder()
+                    .withProtocol("https")
+                    .withDomain("google")
+                    .withPort("8080")
+                    .withPath("search")
+                    .withPath("searchPicture")
+                    .withParam("query")
+                    .withParam("enabled", "false")
+                    .build();
 
-    }
+            String url1 = new TestUrl.Builder()
+                    .withParam("query")
+                    .withPath("search")
+                    .withParam("enabled", "false")
+                    .withProtocol("https://")
+                    .withDomain("google")
+                    .withPort("8080")
+                    .withPath("searchPicture")
+                    .build();
 
-    public String getProtocol() {
-        return protocol;
-    }
+            String url2 = new TestUrl.Builder()
+                    .withPath("search")
+                    .withParam("enabled", "false")
+                    .withProtocol("https://")
+                    .withDomain("google")
+                    .withPort("8080")
+                    .withPath("searchPicture")
+                    .build();
 
-    public void setProtocol(String protocol) {
-        this.protocol = protocol;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getParam() {
-        return param;
-    }
-
-    public void setParam(String param) {
-        this.param = param;
-    }
-
-    public static void main(String[] args) {
-        TestUrl url = new TestUrl.Builder()
-                .withProtocol("https://")
-                .withDomain("google")
-                .withPort("[:8800]")
-                .withParam("=search")
-                .withParam("&123=true","|| false")
-                .build();
-        String s = String.valueOf(url);
-        System.out.println(s);
+            System.out.println(url);
+            System.out.println(url1);
+            System.out.println(url2);
+        }
     }
 }
