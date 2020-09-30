f398223ff5d1d562b6b449ee59c2c0a0156b954e
OriekhovKM <kiradnepr@gmail.com>
2020-09-30 19:44:59 +0300

    Commented code was deleted.

--- src/main/java/Infrastructure/wdm/WebDriverManager.java
+++ src/main/java/Infrastructure/wdm/WebDriverManager.java
@@ -6,22 +6,4 @@ public interface WebDriverManager {
     public String getBrowser();
     public void destroyBrowser(String browser);
 
-
-//    public String createBrowser() {
-//        String testBrowser = ConfigurationManager.getInstance().getTestBrowser();
-//        switch (testBrowser) {
-//            case "chrome":
-//                return "new Google chrome driver";
-//            case "firefox":
-//                return "new Mozilla firefox driver";
-//            default:
-//                return "";
-//        }
-//    }
-//
-//    public void destroyBrowser(String browser) {
-//        if (browser != null) {
-//            System.out.println("Browser closed");
-//        }
-//    }
 }
