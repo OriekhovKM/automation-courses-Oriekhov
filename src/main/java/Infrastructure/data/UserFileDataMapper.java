5f7bf6d9c3c18db2b560e0a502bb871b9ee48a42
OriekhovKM <kiradnepr@gmail.com>
2020-10-16 23:01:49 +0300

    Massive of users was changed to ArrayList

--- src/main/java/Infrastructure/data/UserFileDataMapper.java
+++ src/main/java/Infrastructure/data/UserFileDataMapper.java
@@ -1,22 +1,20 @@
 package Infrastructure.data;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class UserFileDataMapper implements UserDataMapper {
-    User[] users = new User[10];
+    ArrayList users = new ArrayList(10);
+   // ArrayList splitedUser = new ArrayList();
 
     public UserFileDataMapper() {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File("users.txt")))) {
             String rb = null;
+            int i = 0;
             while ((rb = reader.readLine()) != null) {
                 String[] splitedUser = rb.split(";");
-
-                for (int i = 0; i < users.length; i++) {
-                    if (users[i] == null) {
-                        users[i] = new User(splitedUser[0], splitedUser[1], splitedUser[2], splitedUser[3]);
-                        break;
-                    }
-                }
+                users.add(i, new User(splitedUser[0], splitedUser[1], splitedUser[2], splitedUser[3]));
+                i++;
             }
 
         } catch (FileNotFoundException e) {
@@ -28,29 +26,33 @@ public class UserFileDataMapper implements UserDataMapper {
 
     @Override
     public User findUserByEmail(String email) throws UserNotFoundException {
-
-           for ( User user : users) {
-               if (user != null && user.getUserEmail().equals(email)) {
+        User user;
+        for (int i = 0; i < users.size(); i++) {
+           user = (User) users.get(i);
+            if (user != null && user.getUserEmail().equals(email)) {
                    return user;
-               }
-           }
+             }
+        }
            throw new UserNotFoundException(String.format("User with email '%s' not found ", email));
     }
 
 
     @Override
     public User findUserByName(String name) throws UserNotFoundException {
-            for (User user : users) {
-
-                if (user != null && user.getUserName().equals(name)) {
-                    return user;
-                }
-            }
-                throw new UserNotFoundException(String.format("User with name '%s' not found", name));
-            }
-
-            public User[] getAll () {
-                return users;
+        User user;
+        for (int i = 0; i < users.size(); i++) {
+            user = (User) users.get(i);
+            if (user != null && user.getUserName().equals(name)) {
+                return user;
             }
         }
+        throw new UserNotFoundException(String.format("User with name '%s' not found", name));
+    }
 
+
+
+        public ArrayList getAll () {
+            return users;
+        }
+
+    }
--- src/main/java/Lectures/Lesson_16/LessonApp.java
+++ src/main/java/Lectures/Lesson_16/LessonApp.java
@@ -5,6 +5,7 @@ import Infrastructure.data.UserFileDataMapper;
 import Infrastructure.data.UserNotFoundException;
 import Lectures.Lesson_16.generic.Box;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LessonApp {
@@ -12,10 +13,11 @@ public class LessonApp {
     public static void main(String[] args) throws UserNotFoundException {
         UserFileDataMapper mapper = new UserFileDataMapper();
 
-        String name = "user 3";
+
+        String name = "user 1";
         String email ="user9@gmail.com";
 
-
+        System.out.println(mapper.getAll());
         System.out.println(mapper.findUserByEmail(email));
         System.out.println(mapper.findUserByName(name));
 
