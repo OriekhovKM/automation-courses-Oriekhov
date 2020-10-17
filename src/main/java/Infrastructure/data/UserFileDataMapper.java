77d41104e59c20df64b58a3a258c59bfe6f7700d
OriekhovKM <47945262+OriekhovKM@users.noreply.github.com>
2020-10-17 20:57:22 +0300

    Home work14 (#13)
    * Methods for finding were added.
    
    * UserFileDataMapper and UserNotFoundException were changed
    
    * Exception was changed according to commits
    
    * Massive of users was changed to ArrayList
    
    * ArrayList users changed to List<User> users.

--- src/main/java/Infrastructure/data/UserFileDataMapper.java
+++ src/main/java/Infrastructure/data/UserFileDataMapper.java
@@ -2,21 +2,22 @@ package Infrastructure.data;
 
 import java.io.*;
 
-public class UserFileDataMapper implements UserDataMapper {
-    User[] users = new User[10];
+import java.util.ArrayList;
+import java.util.List;
 
+public class UserFileDataMapper implements UserDataMapper {
+    List<User> users = new ArrayList();
+  
     public UserFileDataMapper() {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File("users.txt")))) {
             String rb = null;
+
+            int i = 0;
             while ((rb = reader.readLine()) != null) {
                 String[] splitedUser = rb.split(";");
+                users.add(i, new User(splitedUser[0], splitedUser[1], splitedUser[2], splitedUser[3]));
+                i++;
 
-                for (int i = 0; i < users.length; i++) {
-                    if (users[i] == null) {
-                        users[i] = new User(splitedUser[0], splitedUser[1], splitedUser[2], splitedUser[3]);
-                        break;
-                    }
-                }
             }
 
         } catch (FileNotFoundException e) {
@@ -29,28 +30,31 @@ public class UserFileDataMapper implements UserDataMapper {
     @Override
     public User findUserByEmail(String email) throws UserNotFoundException {
 
-           for ( User user : users) {
-               if (user != null && user.getUserEmail().equals(email)) {
-                   return user;
-               }
-           }
-           throw new UserNotFoundException(String.format("User with email '%s' not found ", email));
+14
+        for (int i = 0; i < users.size(); i++) {
+            users.get(i);
+            if (users.get(i) != null && users.get(i).getUserEmail().equals(email)) {
+                return users.get(i);
+            }
+        }
+        throw new UserNotFoundException(String.format("User with email '%s' not found ", email));
+
     }
 
 
     @Override
     public User findUserByName(String name) throws UserNotFoundException {
-            for (User user : users) {
 
-                if (user != null && user.getUserName().equals(name)) {
-                    return user;
-                }
-            }
-                throw new UserNotFoundException(String.format("User with name '%s' not found", name));
-            }
-
-            public User[] getAll () {
-                return users;
+        for (int i = 0; i < users.size(); i++) {
+            users.get(i);
+            if (users.get(i) != null && users.get(i).getUserName().equals(name)) {
+                return users.get(i);
             }
         }
+        throw new UserNotFoundException(String.format("User with name '%s' not found", name));
+    }
 
+    public ArrayList getAll() {
+        return (ArrayList) users;
+    }
+}
